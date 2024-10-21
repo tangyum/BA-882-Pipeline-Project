751005a7106d15d08dbd562507c6dac43d12e5af
Shayanhasan99 <shayanhk@bu.edu>
2024-10-20 21:04:44 -0400

    updated flow (prefect)

--- cloudrun_functions/deploy-etl.py
+++ cloudrun_functions/deploy-etl.py
@@ -3,12 +3,12 @@ from prefect import flow
 if __name__ == "__main__":
     flow.from_source(
         source="https://github.com/tangyum/BA-882-Pipeline-Project.git",
-        entrypoint="cloudrun_functions/etl.py:your_main_function",  # Adjust based on your main function
+        entrypoint="cloudrun_functions/etl.py:etl_flow",  # Adjust based on your main function
     ).deploy(
         name="cloud-functions-deployment",
         work_pool_name="shayan-lab6-pool1",
         job_variables={"env": {"Shayan": "value"}, "pip_packages": ["pandas", "requests"]},
-        cron="15 0 * * *",  # Runs every hour, adjust as needed
+        cron="0 21 * * *",  # Runs every hour, adjust as needed
         tags=["prod"],
         description="Deploys cloud functions to execute tasks on schedule.",
         version="1.0.0",
